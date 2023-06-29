dacf65d2fa2b5637a08ebc8e55e65588f0367a03
eilsonwen <613476@qq.com>
2023-06-29 16:00:46 +0800

    聚类数据切换方法的可视化

--- dm_front_end/src/views/demo/kmeans.vue
+++ dm_front_end/src/views/demo/kmeans.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="app-container">
     <template>
-      <div class="demo-input-suffix">
-        <el-row>
-          <el-select v-model="value" clearable placeholder="请选择">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
-          <el-button type="primary" @click="showChart()">数据 聚类</el-button>
-        </el-row>
-        <el-row>
-          <el-input
-            v-model="input1"
-            size="medium"
-            placeholder="请输入k"
-            clearable
-            class="outer-container"
-          />
-        </el-row>
-      </div>
-    </template>
-    <template>
       <div>
         <el-row>
           <el-col :span="4">
@@ -65,48 +41,71 @@
             @next-click="loadTable"
           />
         </el-row>
-        <div class="demo-input-suffix">
-          <h1 class="support">
-            <el-input
-              v-model="input1"
-              size="medium"
-              placeholder="请输入k"
-              clearable
-            />
-          </h1>
-          <el-button type="primary" @click="showChart()">散点图</el-button>
-        </div>
+      </div>
+      <div class="demo-input-suffix">
+        <el-select v-model="value" class="select-component" clearable placeholder="请选择" @change="test()">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+        <el-button type="primary" class="select-component" @click="showChart()">数据 聚类</el-button>
+        <el-input
+          v-if="value==='选项1'"
+          v-model="input_k"
+          size="medium"
+          placeholder="请输入k"
+          clearable
+          class="outer-container"
+        />
+        <el-input
+          v-if="value==='选项2'"
+          v-model="input_epsilon"
+          size="medium"
+          placeholder="请输入 epsilon"
+          clearable
+          class="outer-container"
+        />
+        <el-input
+          v-if="value==='选项2'"
+          v-model="input_minPts"
+          size="medium"
+          placeholder="请输入 minPts"
+          clearable
+          class="outer-container"
+        />
       </div>
       <div>
-        <!--        <el-col :span="1">-->
-        <!--        </el-col>-->
-        <el-col :span="20">
+        <el-col :span="12">
+          <el-slider
+            v-model="value2"
+            :step="1"
+            max="30"
+            height="200px"
+            show-stops
+          />
+        </el-col>
+        <el-col :span="24">
           <el-row>
             <el-col :span="8">
               <div class="chart-container ">
-                <div id="chart1" class="chart" />
+                <div id="1" class="chart" />
               </div>
             </el-col>
             <el-col :span="8">
               <div class="chart-container ">
-                <div id="chart2" class="chart" />
-              </div>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="8">
-              <div class="chart-container ">
-                <div id="chart3" class="chart" />
+                <div id="2" class="chart" />
               </div>
             </el-col>
             <el-col :span="8">
               <div class="chart-container ">
-                <div id="chart4" class="chart" />
+                <div id="3" class="chart" />
               </div>
             </el-col>
           </el-row>
         </el-col>
-
       </div>
     </template>
   </div>
@@ -136,25 +135,18 @@ export default {
       showErrorDialog: false,
       isFocused: false,
       isEditing: false,
-      input1: '',
-      input2: '',
+      input_k: '',
+      input_epsilon: '',
+      input_minPts: '',
       pageNum: 1,
       pageSize: 8,
+      value: '',
       options: [{
         value: '选项1',
-        label: '黄金糕'
+        label: 'kmeans'
       }, {
         value: '选项2',
-        label: '双皮奶'
-      }, {
-        value: '选项3',
-        label: '蚵仔煎'
-      }, {
-        value: '选项4',
-        label: '龙须面'
-      }, {
-        value: '选项5',
-        label: '北京烤鸭'
+        label: 'dbscan'
       }],
       columns: [
         { prop: 'id', label: 'id' },
@@ -166,27 +158,11 @@ export default {
       ]
     }
   },
+
   mounted() {
-    this.renderChart('chart1', [
-      [1, 2],
-      [3, 4],
-      [5, 6]
-    ])
-    this.renderChart('chart2', [
-      [7, 8],
-      [9, 10],
-      [11, 12]
-    ])
-    this.renderChart('chart3', [
-      [13, 14],
-      [15, 16],
-      [17, 18]
-    ])
-    this.renderChart('chart4', [
-      [19, 20],
-      [21, 22],
-      [23, 24]
-    ])
+    this.renderChart('1', [])
+    this.renderChart('2', [])
+    this.renderChart('3', [])
   },
   methods: {
     loadTable(num) {
@@ -195,15 +171,13 @@ export default {
         this.tableData = res.data.list
       })
     },
+    test() {
+      this.renderChart('1', [])
+      this.renderChart('2', [])
+      this.renderChart('3', [])
+    },
     checkRowData(row) {
       return row.name !== '' && row.age !== null
-    },
-    show() {
-      this.tableData = [
-        { name: '张三', age: 20, editingFields: [] },
-        { name: '李四', age: 25, editingFields: [] },
-        { name: '王五', age: 30, editingFields: [] }
-      ]
     },
     startEditing() {
       this.isEditing = true
@@ -221,22 +195,31 @@ export default {
       this.tableData.push({ name: '', age: null })
     },
     renderChart(containerId, data) {
-      axios.get('http://localhost:8080/api/clu_test?K_num=' + this.input1).then(res => {
+      var url
+      if (this.value === '选项1') {
+        url = 'http://localhost:8080/api/kmeans?K_num=' + this.input_k + '&dataset=' + containerId
+      } else {
+        url = 'http://localhost:8080/api/dbscan?dataset=' + containerId + '&epsilon=' + this.input_epsilon + '&minPts=' + this.input_minPts
+      }
+      axios.get(url).then(res => {
         this.pieces_raw_data = res.data
         const echarts = require('echarts')
         const chart = echarts.init(document.getElementById(containerId))
-        // var colors = [
-        //   '#37A2DA',
-        //   '#e06343',
-        //   '#37a354',
-        //   '#b55dba',
-        //   '#b5bd48',
-        //   '#8378EA',
-        //   '#96BFFF'
-        // ]
+        var colors = [
+          '#37A2DA',
+          '#e06343',
+          '#37a354',
+          '#b55dba',
+          '#b5bd48',
+          '#8378EA',
+          '#96BFFF'
+        ]
         var data = []
         for (var i = 0; i < this.pieces_raw_data.length; i++) {
-          data.push({ value: [this.pieces_raw_data[i].x, this.pieces_raw_data[i].y], category: this.pieces_raw_data[i].clu_res })
+          data.push({
+            value: [this.pieces_raw_data[i].x, this.pieces_raw_data[i].y],
+            category: this.pieces_raw_data[i].clu_res
+          })
         }
         const option = {
           xAxis: {
@@ -251,41 +234,6 @@ export default {
               text: 'Gradient along the y axis'
             }
           ],
-          series: [{
-            type: 'scatter',
-            data: data,
-            symbolSize: 10 // 设置散点的大小
-          }]
-        }
-        chart.setOption(option)
-      })
-    },
-    showChart() {
-      axios.get('http://localhost:8080/api/clu_test?K_num=' + this.input1).then(res => {
-        this.pieces_raw_data = res.data
-        const echarts = require('echarts')
-        document.getElementById('echart1').removeAttribute('_echarts_instance_')
-        const myChart = echarts.init(document.getElementById('echart1'), 'walden')
-        var colors = [
-          '#37A2DA',
-          '#e06343',
-          '#37a354',
-          '#b55dba',
-          '#b5bd48',
-          '#8378EA',
-          '#96BFFF'
-        ]
-        var data = []
-        for (var i = 0; i < this.pieces_raw_data.length; i++) {
-          data.push({ value: [this.pieces_raw_data[i].x, this.pieces_raw_data[i].y], category: this.pieces_raw_data[i].clu_res })
-        }
-        const option = {
-          xAxis: {
-            type: 'value',
-            min: -4, // 设置 x 轴的最小值
-            max: 4 // 设置 x 轴的最大值},
-          },
-          yAxis: {},
           series: data.map(item => ({
             type: 'scatter',
             data: [item.value],
@@ -293,9 +241,16 @@ export default {
               color: colors[item.category] // 根据类别设置颜色
             }
           }))
+
         }
-        myChart.setOption(option)
+        chart.setOption(option)
       })
+    },
+    showChart() {
+      this.renderChart('1', [])
+      this.renderChart('2', [])
+      this.renderChart('3', [])
+      this.renderChart('4', [])
     }
   }
 }
@@ -304,33 +259,31 @@ export default {
 
 <style scoped>
 .demo-input-suffix {
+  //border: 1px solid #ccc;
   gap: 10px;
   align-items: center;
-  margin-left: 0;
-  margin-top: 10px;
+  margin-right: 0;
+  margin-top: 50px;
 
 }
 
-#echart1 {
-  margin-top: 0px;
-  left: 0;
-  height: 100vh;
-}
 .chart-container {
-  height: 40vh;
+  height: 50vh;
   width: 50vh;
   margin: -1px; /* 为了消除列之间的间距 */
-//display: flex;
   align-items: center;
   justify-content: center;
 }
 .outer-container {
   margin-top: 5px;
-  width: 300px;
+  width: 315px;
 
 }
 .chart {
   width: 100%;
   height: 100%;
+}
+.select-component {
+  margin-right: 10px; /* 调整组件之间的水平间距 */
 }
 </style>
